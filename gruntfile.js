29bada65b127d131a2242a940cbd1f0ab907b7a3
David Furnes <dfurnes@dosomething.org>
2013-08-08 17:28:55 -0400

    Fixed wrong path for package.json.

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('etc/package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			sass: {
 				files: ['scss/**/*.{scss,sass}'],
