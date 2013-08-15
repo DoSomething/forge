5aafcb964a3609b0775d365659690b6b6d6ebf12
David Furnes <dfurnes@dosomething.org>
2013-08-15 14:09:52 -0400

    Styleguide Neue.

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'kss'
+
+set :public_folder, Proc.new { File.join(root, "assets") }
+
+get '/' do
+  @styleguide = Kss::Parser.new('scss/')
+  erb :styleguide
+end
+
+get '/*.html' do
+  send_file "views/static_examples/#{params[:splat].first}.html"
+end
+
+helpers do
+  # Generates a styleguide block. A little bit evil with @_out_buf, but
+  # if you're using something like Rails, you can write a much cleaner helper
+  # very easily.
+  def styleguide_block(section, &block)
+    @section = @styleguide.section(section)
+    @example_html = capture{ block.call }
+    @escaped_html = ERB::Util.html_escape @example_html
+    @_out_buf << erb(:_styleguide_block)
+  end
+
+  # Captures the result of a block within an erb template without spitting it
+  # to the output buffer.
+  def capture(&block)
+    out, @_out_buf = @_out_buf, ""
+    yield
+    @_out_buf
+  ensure
+    @_out_buf = out
+  end
+end
