5d394f9f4d8c4cbc342706bf977a1c4971be81c1
David Furnes <dfurnes@dosomething.org>
2013-11-14 14:20:28 -0500

    Added documentation links to styleguide, example module documentation.

--- js/__exampleModule.js
+++ js/__exampleModule.js
@@ -1,56 +1,65 @@
-/**
+//
+//
+// Give a brief description of what your module does. Usage:
+// 
+// ```js
+// window.DS.ExampleModule.initialize({
+//   configuration_option: true
+// });
+//
+// exampleModule.getStatus();
+// ```
+//
+// 
 
-  Give a brief description of what your module does. Usage:
- 
-  ```js
-  window.DS.ExampleModule.initialize({
-    configuration_option: true
-  });
-
-  exampleModule.getStatus();
-  ```
-
-*/
-
+// We create the global DS namespace if it doesn't already exist, and attach our module to it.
 window.DS = window.DS || {};
 window.DS.ExampleModule = function() {
   'use strict';
 
-  // PRIVATE METHODS & VARIABLES:
+  // We can track whether a module has been initialized or not in the `initialized` variable.
   var initialized = false;
   var options = {};
 
-  function status() {
-    // public method to report status
+  function privateFunction() {
+    
   }
 
-  //  PUBLIC METHODS:
-  return {
-    initialize: function(opts) {
+  function anotherPrivateFunction() {
+    
+  }
+
+  function public__Initialize() {
+    function(opts) {
       var defaults = {
         configurationOption: false
       };
 
-      // Override default options with any settings passed during initialization.
+      // We override default options with any settings passed during initialization:
       if ((typeof opts !== 'undefined' && opts !== null)) {
         options = $.extend({}, defaults, opts);
       } else {
         options = defaults;
       }
 
-      // Do anything else we need to set up this module.
       // ...
 
-      status();
-
       initialized = true;
-    },
-
-    getStatus: function() {
-      if(initialized) {
-        status();
-      }
     }
+  }
 
+  function public__getStatus() {
+    
+    // We can use the `initialized` varible to ensure that the module has been initialized before other methods can run.
+    if(initialized) {
+      alert("We're ready to go!")
+    }
+  }
+
+  // We use the [Revealing Module pattern](http://addyosmani.com/resources/essentialjsdesignpatterns/book/#revealingmodulepatternjavascript)
+  // to expose public methods while keeping private methods/variables safe.
+  return {
+    initialize: public__Initialize,
+    getStatus: public__getStatus
   };
 };
