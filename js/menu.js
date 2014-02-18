7342885e5bca87004c71773841c81663393e4b00
David Furnes <dfurnes@dosomething.org>
2014-02-18 11:24:24 -0500

    Mobile styling for footer.

--- js/menu.js
+++ js/menu.js
@@ -13,14 +13,15 @@
       $(".nav--wrapper .main").toggleClass("is-visible");
     });
 
-    // Hide utility bar items when search field is focused:
-    $(".secondary-nav input[type='search']").on("focus", function() {
-      $(".secondary-nav-item").addClass("is-hidden-on-tablets");
+    // Hide footer on mobile until clicked
+    $(".footer--wrapper .col").addClass("is-collapsed");
+    $(".js-footer-col h4").on("click", function() {
+      if( matchMedia("screen and (max-width: 768px)").matches ) {
+        $(this).closest(".col").toggleClass("is-collapsed");
+        console.log('do it');
+      }
     });
 
-    $(".secondary-nav input[type='search']").on("blur", function() {
-      $(".secondary-nav-item").removeClass("is-hidden-on-tablets");
-    });
   });
 
 })(jQuery);
