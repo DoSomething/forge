bfdad6b2a78a6d2f8f536dbb729c4b08b12bdb88
David Furnes <dfurnes@dosomething.org>
2014-02-20 14:45:48 -0500

    Updated tests with new Validation function object.

--- tests/tests.js
+++ tests/tests.js
@@ -3,11 +3,11 @@
 module("Validation");
 
 test("Name", function() {
-  ValidationFunctions.name("John", function(result) {
+  NEUE.Validation.Functions.name("John", function(result) {
     ok(result.message == "Hey, John!", "should greet");
   });
 
-  ValidationFunctions.name("", function(result) {
+  NEUE.Validation.Functions.name("", function(result) {
     ok(result.success == false, "should require name");
   });
 
@@ -18,27 +18,27 @@ test("Birthdays", function() {
   var birthday_date = (now.getMonth() + 1) + "/" + now.getDate() + "/1990";
   var user_19_years_old = Math.min(now.getMonth() + 2, 12) + "/" + now.getDate() + "/" + (now.getFullYear() - 20);
 
-  ValidationFunctions.birthday(birthday_date, function(result) {
+  NEUE.Validation.Functions.birthday(birthday_date, function(result) {
     ok(result.message == "Wow, happy birthday!", "should wish me a happy birthday");
   });
 
-  ValidationFunctions.birthday(user_19_years_old, function(result) {
+  NEUE.Validation.Functions.birthday(user_19_years_old, function(result) {
     ok(result.message == "Cool, 19!", "should tell me my age");
   });
 
-  ValidationFunctions.birthday("yesterday", function(result) {
+  NEUE.Validation.Functions.birthday("yesterday", function(result) {
     ok(result.success == false, "should reject non-formatted dates");
   });
 
-  ValidationFunctions.birthday("1/15/1960", function(result) {
+  NEUE.Validation.Functions.birthday("1/15/1960", function(result) {
     ok(result.message == "Yikes, you're old!", "should tell old people that they're old");
   });
 
-  ValidationFunctions.birthday("1/15/1860", function(result) {
+  NEUE.Validation.Functions.birthday("1/15/1860", function(result) {
     ok(result.message == "That doesn't seem right.", "should reject users with birthdates too far in the past");
   });
 
-  ValidationFunctions.birthday("1/15/2095", function(result) {
+  NEUE.Validation.Functions.birthday("1/15/2095", function(result) {
     ok(result.message == "Are you a time traveller?", "should reject users with future birthdates");
   });
 
@@ -46,30 +46,30 @@ test("Birthdays", function() {
 
 
 test("Emails", function() {
-  ValidationFunctions.email("my.name@gmail.com", function(result) {
+  NEUE.Validation.Functions.email("my.name@gmail.com", function(result) {
     ok(result.success == true, "should accept a valid email");
   });
 
-  ValidationFunctions.email("my.name.com", function(result) {
+  NEUE.Validation.Functions.email("my.name.com", function(result) {
     ok(result.success == false, "should reject an invalid email");
   });
 
-  ValidationFunctions.email("myname@com", function(result) {
+  NEUE.Validation.Functions.email("myname@com", function(result) {
     ok(result.success == false, "should reject an invalid email");
   });
 
-  ValidationFunctions.email("myname@gmail.123", function(result) {
+  NEUE.Validation.Functions.email("myname@gmail.123", function(result) {
     ok(result.success == false, "should reject an invalid email");
   });
 });
 
 
 test("Passwords", function() {
-  ValidationFunctions.password("12345", function(result) {
+  NEUE.Validation.Functions.password("12345", function(result) {
     ok(result.success == false, "should reject if too short");
   });
 
-  ValidationFunctions.password("super$ecure19", function(result) {
+  NEUE.Validation.Functions.password("super$ecure19", function(result) {
     ok(result.success == true, "should be totally chill with a good password");
   });
 });
