65397be1dc049accabade6a58ed935d02d75e377
David Furnes <dfurnes@dosomething.org>
2014-03-31 14:31:43 -0400

    Prepared for distribution.

--- tasks/lint.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(grunt) {
-  grunt.registerTask("lint", ["lint:js", "lint:css"]);
-
-  grunt.registerTask("lint:js", ["jshint:all", "lintspaces:js"]);
-  grunt.registerTask("lint:css", ["shell:scsslint", "lintspaces:scss"]);
-}
