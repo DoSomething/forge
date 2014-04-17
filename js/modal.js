b5660e549b615c98af67eeb6372b94ccfbf54180
David Furnes <dfurnes@dosomething.org>
2014-04-17 12:18:22 -0400

    Close modals on clicking overlay. Restructured modal code to be a bit cleaner.

--- js/modal.js
+++ js/modal.js
@@ -15,21 +15,29 @@ var NEUE = NEUE || {};
 (function($) {
   "use strict";
 
-  var modalIsOpen = false;
-  var $modal, $modalContent;
+  NEUE.Modal = {
+    // We can only have one modal open at a time; we track that here.
+    modalIsOpen: false,
 
-  $(document).ready(function() {
-    // Trigger modal on click:
-    $("body").on("click", ".js-modal-link", function(e) {
-      e.preventDefault();
+    // The modal container (including background overlay).
+    $modal: null,
 
-      var href;
+    // The content of the modal.
+    $modalContent: null,
+
+    // Stores the "path" of the current modal
+    href: "",
+
+    // Open a new modal
+    open: function(event) {
+      event.preventDefault();
+
       if( $(this).data("cached-modal") ) {
-        href = $(this).data("cached-modal");
-      } else if ( e.target.hash.charAt(0) === "#"  ) {
+        NEUE.Modal.href = $(this).data("cached-modal");
+      } else if ( event.target.hash.charAt(0) === "#"  ) {
         // We find the modal based on the ID in the link"s `href`. For example,
         // `<a class="js-modal-link" href="#modal--faq">Click me</a>` would open `<div id="modal--faq"></div>`.
-        href = $(e.target.hash);
+        NEUE.Modal.href = $(event.target.hash);
       } else {
         // @TODO: We should handle AJAX loading things in.
       }
@@ -37,25 +45,29 @@ var NEUE = NEUE || {};
       // If Google Analytics is set up, we fire an event to track that a
       // modal has been opened.
       if(typeof(_gaq) !== "undefined" && _gaq !== null) {
-        _gaq.push(["_trackEvent", "Modal", "Open", href, null, true]);
+        _gaq.push(["_trackEvent", "Modal", "Open", NEUE.Modal.href, null, true]);
       }
 
-      if( !modalIsOpen ) {
+      if( !NEUE.Modal.modalIsOpen ) {
         // create modal in DOM
-        $modal = $("<div class=\"modal\"></div>");
-        $modalContent = $("<div class='modal-content'></div>");
-        $modal.append($modalContent);
-        $modalContent.html( $(href).html() );
+        NEUE.Modal.$modal = $("<div class=\"modal\"></div>");
+        NEUE.Modal.$modalContent = $("<div class='modal-content'></div>");
+        NEUE.Modal.$modal.append(NEUE.Modal.$modalContent);
+        NEUE.Modal.$modalContent.html( $(NEUE.Modal.href).html() );
 
         // set up overlay and show modal
         $("body").addClass("modal-open");
-        $("body").append($modal);
-        $modal.addClass("fade-in");
-        $modalContent.addClass("fade-in-up");
-        $modalContent.addClass( $(href).attr("class") );
-        $modal.show();
+        $("body").append(NEUE.Modal.$modal);
+        NEUE.Modal.$modal.addClass("fade-in");
+        NEUE.Modal.$modalContent.addClass("fade-in-up");
+        NEUE.Modal.$modalContent.addClass( $(NEUE.Modal.href).attr("class") );
+        NEUE.Modal.$modal.show();
 
-        modalIsOpen = true;
+        // Bind events to close Modal
+        NEUE.Modal.$modal.on("click", ".js-close-modal", NEUE.Modal.close);
+        NEUE.Modal.$modal.on("click", NEUE.Modal.close);
+
+        NEUE.Modal.modalIsOpen = true;
 
         //  **This fixes an issue with `position:fixed` and the virtual keyboard
         //  on Mobile Safari.** Since this is a browser bug, we're forced to use
@@ -63,47 +75,59 @@ var NEUE = NEUE || {};
         //  as this is fixed in the future. Yes, it is gross.
         if(  /iPhone|iPad|iPod/i.test(window.navigator.userAgent) ) {
           setTimeout(function () {
-            $modal.css({ "position": "absolute", "overflow": "visible", "height": $(document).height() + "px" });
-            $modalContent.css({ "margin-top": $(document).scrollTop() + "px" });
+            NEUE.Modal.$modal.css({ "position": "absolute", "overflow": "visible", "height": $(document).height() + "px" });
+            NEUE.Modal.$modalContent.css({ "margin-top": $(document).scrollTop() + "px" });
           }, 0);
         }
       } else {
         // modal is already open, so just replace current content
-        $modalContent.html( $(href).html() );
+        NEUE.Modal.$modalContent.html( $(NEUE.Modal.href).html() );
       }
 
       // We'll set up form validation markup for anything in the modal (since it isn't in the DOM on load)
       // @TODO: Should be providing an event that other modules can hook into (so the Validation Module would take care of this).
-      NEUE.Validation.prepareFormLabels($modalContent);
+      NEUE.Validation.prepareFormLabels(NEUE.Modal.$modalContent);
+    },
 
-      // Close modal when "x" is clicked:
-      $modal.on("click", ".js-close-modal", function(e) {
-        e.preventDefault();
+    // Close modal
+    close: function(event) {
+      // Don't let the event bubble.
+      if(event.target !== this) {
+        return;
+      }
 
-        // If Google Analytics is set up, we fire an event to track that a
-        // modal has been closed.
-        if(typeof(_gaq) !== "undefined" && _gaq !== null) {
-          _gaq.push(["_trackEvent", "Modal", "Close", href, null, true]);
-        }
+      // Override default link behavior.
+      event.preventDefault();
 
-        if(Modernizr.cssanimations) {
-          $modalContent.addClass("fade-out-down");
-          $modal.addClass("fade-out");
+      // If Google Analytics is set up, we fire an event to track that a
+      // modal has been closed.
+      if(typeof(_gaq) !== "undefined" && _gaq !== null) {
+        _gaq.push(["_trackEvent", "Modal", "Close", NEUE.Modal.href, null, true]);
+      }
 
-          $("body").removeClass("modal-open");
+      if(Modernizr.cssanimations) {
+        NEUE.Modal.$modalContent.addClass("fade-out-down");
+        NEUE.Modal.$modal.addClass("fade-out");
 
-          $modal.one("webkitAnimationEnd oanimationend msAnimationEnd animationend", function() {
-            $modal.remove();
-            modalIsOpen = false;
-          });
-        } else {
-          $("body").removeClass("modal-open");
-          $modal.remove();
-        }
+        $("body").removeClass("modal-open");
 
-      });
-    });
+        NEUE.Modal.$modal.one("webkitAnimationEnd oanimationend msAnimationEnd animationend", function() {
+          NEUE.Modal.$modal.remove();
+          NEUE.Modal.modalIsOpen = false;
+        });
+      } else {
+        $("body").removeClass("modal-open");
+        NEUE.Modal.$modal.remove();
+        NEUE.Modal.modalIsOpen = false;
+      }
+    }
+  };
 
+
+  $(document).ready(function() {
+    // Trigger modal on click:
+    $("body").on("click", ".js-modal-link", NEUE.Modal.open);
+
+    // Close modal events are bound on modal initialization.
   });
-
 })(jQuery);
