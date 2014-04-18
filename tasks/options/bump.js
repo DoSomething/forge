fa72dc64b8d46e974c12591fe9802d5305284e0e
David Furnes <dfurnes@dosomething.org>
2014-04-18 12:11:28 -0400

    Prepared for distribution.

--- tasks/options/bump.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  options: {
-    files: ["package.json", "bower.json"],
-    commitFiles: ["package.json", "bower.json"],
-    updateConfigs: ["pkg"],
-    pushTo: "origin",
-    createTag: false
-  }
-}
