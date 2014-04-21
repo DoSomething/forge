2167f4ad5032c8e7049748cbc87b90f701470527
David Furnes <dfurnes@dosomething.org>
2014-04-21 14:48:54 -0400

    Using RequireJS and Almond for JS build.

---
+++ tasks/options/requirejs.js
@@ -0,0 +1,12 @@
+module.exports = {
+  compile: {
+    options: {
+      baseUrl: "js",
+      name: "../bower_components/almond/almond",
+      include: "main",
+      out: "dist/neue.js",
+      optimize: "none"
+    }
+
+  }
+}
