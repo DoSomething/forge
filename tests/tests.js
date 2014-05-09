89e95c52f845787f44171943447f59ebb83cb359
David Furnes <dfurnes@dosomething.org>
2014-05-09 12:24:49 -0400

    Fix tests to use new Validation container.

--- tests/tests.js
+++ tests/tests.js
@@ -3,19 +3,19 @@
 module("Validation");
 
 test("Match", function() {
-  window.NEUE.Validation.Functions.match("dog", "cat", function(result) {
+  window.NEUE.Validation.Validations.match.fn("dog", "cat", function(result) {
     ok(result.success == false, "should reject if different strings");
   });
 
-  window.NEUE.Validation.Functions.match("dog", "Dog", function(result) {
+  window.NEUE.Validation.Validations.match.fn("dog", "Dog", function(result) {
     ok(result.success == false, "should reject if different capitalization");
   });
 
-  window.NEUE.Validation.Functions.match("dog", "dog ", function(result) {
+  window.NEUE.Validation.Validations.match.fn("dog", "dog ", function(result) {
     ok(result.success == false, "should reject if different spacing");
   });
 
-  window.NEUE.Validation.Functions.match("super$ecure19", "super$ecure19", function(result) {
+  window.NEUE.Validation.Validations.match.fn("super$ecure19", "super$ecure19", function(result) {
     ok(result.success == true, "should accept if the same");
   });
 });
