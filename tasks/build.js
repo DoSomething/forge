aaca602c1d87be3123adb0fda3df4cdf8da6c928
David Furnes <dfurnes@dosomething.org>
2014-05-13 17:01:34 -0400

    Prepared for distribution.

--- tasks/build.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(grunt) {
-  // The `build` task compiles assets for local development (unminified, with debugging comments).
-  grunt.registerTask("build", ["lint", "sass:compile", "requirejs:compile", "copy:main"]);
-
-  // The `prod` build task is used when building for production. Since compiled assets
-  // are ignored in version control, this is run in Continuous Integration on deploy.
-  grunt.registerTask("prod", ["shell:clean", "sass:compile", "cssmin:minify", "copy:main", "requirejs:compile", "uglify:prod"]);
-}
