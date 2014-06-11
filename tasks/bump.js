650e310ecc98e77ada42db70a36d98aa1cc476fa
David Furnes <dfurnes@dosomething.org>
2014-06-11 13:56:14 -0400

    Don't push on bump.

--- tasks/bump.js
+++ tasks/bump.js
@@ -2,8 +2,6 @@ module.exports = {
   options: {
     files: ["package.json", "bower.json"],
     commitFiles: ["package.json", "bower.json"],
-    updateConfigs: ["pkg"],
-    pushTo: "origin",
     createTag: false
   }
 }
