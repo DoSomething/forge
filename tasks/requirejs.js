cf002fb9b94ed7badcfe4451b0c254aad9fc3c2d
David Furnes <dfurnes@dosomething.org>
2014-07-10 15:58:09 -0400

    Remove code optimization step.
    We’re running this in client app optimization step, and it makes it
    impossible to use debugger or console methods for debugging in Neue.

--- tasks/requirejs.js
+++ tasks/requirejs.js
@@ -12,17 +12,7 @@ module.exports = {
       wrap: true,
       preserveLicenseComments: false,
       generateSourceMaps: true,
-      optimize: "uglify2",
-      uglify2: {
-        compress: {
-          dead_code: true,
-          drop_debugger: true,
-          drop_console: true,
-          global_defs: {
-            DEBUG: false
-          }
-        }
-      }
+      optimize: "uglify2"
     }
   }
 }
