638c7ac3192492ad827c4377b14f754d2ed60d99
David Furnes <dfurnes@dosomething.org>
2014-10-08 16:33:27 -0400

    Organize patterns into components, modules, and regions.

--- tasks/sass.js
+++ tasks/sass.js
@@ -2,6 +2,7 @@ module.exports = {
   compile: {
     files: {
       "dist/neue.css": "scss/neue-build.scss",
+      "dist/styleguide.css": "scss/styleguide.scss",
     },
     options: {
       outputStyle: "compressed",
