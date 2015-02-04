09c34db4e7209d536cca216e15572b4150d67c3d
David Furnes <david@dfurnes.com>
2015-02-04 11:21:46 -0500

    Change modernizr build filename.

--- tasks/modernizr.js
+++ tasks/modernizr.js
@@ -1,7 +1,7 @@
 module.exports = {
   all: {
     "devFile": "remote",
-    "outputFile": "dist/modernizr-neue.js",
+    "outputFile": "dist/modernizr.js",
     "files" : {
       "src": [
         "js/**/*.js",
