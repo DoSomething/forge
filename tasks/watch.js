bc87e4c6b5429915aff9b6e90dda6f8ca7b81769
David Furnes <david@dfurnes.com>
2015-02-06 15:56:55 -0500

    Remove QUnit, since unit tests for validation now exist in their own repository.

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,11 +1,11 @@
 module.exports = {
   sass: {
-    files: ["scss/**/*.{scss,sass}"],
+    files: ["scss/**/*.scss"],
     tasks: ["sass", "scsslint",  "postcss"]
   },
   js: {
-    files: ["js/**/*.js", "tests/**/*.js"],
-    tasks: ["requirejs", "jshint", "qunit"]
+    files: ["js/**/*.js"],
+    tasks: ["requirejs", "jshint"]
   },
   assets: {
     files: ["assets/**/*"],
