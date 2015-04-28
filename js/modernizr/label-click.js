9fee5b6164ee30c11938eea9c410dc47bd733214
David Furnes <david@dfurnes.com>
2015-04-28 15:11:49 -0400

    Add try/catch block for cases where click method not supported.

--- js/modernizr/label-click.js
+++ js/modernizr/label-click.js
@@ -13,7 +13,16 @@ Modernizr.addTest("label-click", function() {
   var testInput = document.createElement("input");
   testInput.setAttribute("type", "checkbox");
   testLabel.appendChild(testInput);
-  testLabel.click();
+
+  try {
+    // Trigger a `click` on the label, so we can check if the
+    // corresponding input becomes checked.
+    testLabel.click();
+  } catch(exception) {
+    // If `DOMElement.click()` is not supported, such as in
+    // PhantomJS, just fail the test.
+    return false;
+  }
 
   return testInput.checked;
 });
