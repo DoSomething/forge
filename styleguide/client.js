e86f5c0822d148f4a35dd56e5b0b0dc4e9f1d772
David Furnes <david@dfurnes.com>
2016-03-21 12:02:14 -0400

    Use NPM scripts & shared Webpack config for builds.

---
+++ styleguide/client.js
@@ -0,0 +1,11 @@
+import 'dosomething-modal';
+import 'dosomething-validation';
+import 'fixed-sticky';
+
+/**
+ * Document all the things!
+ */
+import './client.scss';
+
+import './assets/kss';
+import './assets/kss-colortiles';
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,8 @@
-var webpack = require('webpack');
+var config = require('@dosomething/webpack-config');
 
-var config = {
-  entry: './js/index.js',
-  output: {
-    filename: 'dist/forge.js',
-    library: 'Forge',
-    libraryTarget: 'umd'
+module.exports = config({
+  entry: {
+    forge: './index.js',
+    styleguide: './styleguide/client.js'
   },
-  externals: {
-    // Don't bundle the 'jquery' package in forge.js, but
-    // instead load from `jQuery` global variable or AMD/CJS package.
-    'jquery': {
-      root: 'jQuery',
-      commonjs2: 'jquery',
-      amd: 'jquery'
-    }
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': process.env.NODE_ENV
-      }
-    }),
-    // ...
-  ]
-};
-
-if(process.env.NODE_ENV === 'production') {
-  // In production, minify our output with UglifyJS
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        drop_console: true,
-        drop_debugger: true,
-        dead_code: true
-      }
-    })
-  )
-} else {
-  // Enable inline source maps when in development
-  config.devtool = '#inline-source-map';
-}
-
-module.exports = config;
+});
