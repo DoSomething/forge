263d59d049fc6f20c27d4648ab4a03c27ce8578c
David Furnes <david@dfurnes.com>
2018-01-22 13:13:58 -0500

    Update dependencies.
    Notable changes include Webpack 3, jQuery 3, and our shared Babel config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
-var config = require('@dosomething/webpack-config');
+const configure = require('@dosomething/webpack-config');
+const path = require('path');
 
-module.exports = config({
+module.exports = configure({
   entry: {
     forge: './index.js',
     styleguide: './styleguide/styleguide.js'
   },
-  
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+  },
+
   // Don't bundle the 'jquery' package with the library (forge.js), but
   // instead load from `jQuery` global variable or AMD/CJS package.
   externals: {
     'jquery': {
       root: 'jQuery',
+      commonjs: 'jquery',
       commonjs2: 'jquery',
       amd: 'jquery'
     }
