972dc57107378f0dc4d6b26599fa3653d3196aa4
David Furnes <david@dfurnes.com>
2019-06-17 12:31:20 -0400

    Update dependencies & build system.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const configure = require('@dosomething/webpack-config');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 module.exports = configure({
   entry: {
     forge: './index.js',
-    styleguide: './styleguide/styleguide.js'
+    styleguide: './styleguide/styleguide.js',
   },
 
   output: {
     filename: '[name].js',
     path: path.join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'forge',
   },
 
   // Don't bundle the 'jquery' package with the library (forge.js), but
   // instead load from `jQuery` global variable or AMD/CJS package.
   externals: {
-    'jquery': {
+    jquery: {
       root: 'jQuery',
       commonjs: 'jquery',
       commonjs2: 'jquery',
-      amd: 'jquery'
-    }
+      amd: 'jquery',
+    },
   },
 
-  plugins: [
-    new ExtractTextPlugin('[name].css'),
-  ]
+  // Reset extracted CSS name (so we don't include a content hash):
+  plugins: [new MiniCssExtractPlugin({ filename: '[name].css' })],
 });
